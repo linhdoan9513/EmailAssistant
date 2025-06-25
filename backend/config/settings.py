80691b7703226886dbe0095e05fb4b62f6ad62db
linhdoan9513 <doanlinh9513@gmail.com>
2025-06-25 15:08:06 -0400

    set dubg to false

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -35,7 +35,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8o^l_i*@@rkjvwgxwk72d47b^8s0yr*#_^@&!+$rli93eg5zup'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG=True
+DEBUG=False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "emailassistant.onrender.com"]
 
