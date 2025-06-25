92c89bd5405edbc5f36b97a42392fb73589b1e59
linhdoan9513 <doanlinh9513@gmail.com>
2025-06-25 11:05:18 -0400

    add allow host

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8o^l_i*@@rkjvwgxwk72d47b^8s0yr*#_^@&!+$rli93eg5zup
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG=True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "emailassistant.onrender.com"]
 
 
 # Application definition
